2d753703c211efec092377f5fa6ca59c8e9241c6
Arthur Dorchies <166439257+ArtDorc@users.noreply.github.com>
2024-06-06 22:33:28 +0200

    More interactive

--- Game-of-Throne-app/app.js
+++ Game-of-Throne-app/app.js
@@ -1,15 +1,38 @@
-document.querySelectorAll(".imgHolder").forEach(item => item.addEventListener("click", onClick));
+document.querySelectorAll(".imgHolder").forEach(item => {
+    item.addEventListener("click", onClick);
+    item.addEventListener("mouseenter",hoverMouseIn);
+    item.addEventListener("mouseleave",hoverMouseOut);
+});
+
 let charactTarget = document.querySelector(".charact0");
 let previousChar=false;
 
-function onClick(e) {
-    
+function hoverMouseIn(e) {
     e.stopPropagation();
+    e.target.children[0].style.backgroundColor="rgba(0,0,0,0.45)"
+}
+
+function hoverMouseOut(e) {
+    e.stopPropagation();
+    e.target.children[0].style.backgroundColor="rgba(0,0,0,0.75)"
+}
+
+function onClick(e) {
+    e.stopPropagation();
+    //remove hover when clicked
+    e.target.removeEventListener("mouseenter", hoverMouseIn);
+    e.target.removeEventListener("mouseleave", hoverMouseOut);
+
     //reset previous
     charactTarget.style.display="none";
     if (previousChar) {
         previousChar.children[0].style.backgroundColor="rgba(0,0,0,0.75)";
         previousChar.style.transform="scale(1)"
+        //reinstal hover
+        if (previousChar!== e.target) {
+        previousChar.addEventListener("mouseenter",hoverMouseIn);
+        previousChar.addEventListener("mouseleave",hoverMouseOut);
+        }
     }
     //set next one
     previousChar = e.target;
